1152428791db6d580b150d8d8b7a0411bf1f8d01
richardjzhang <rzhang@mathspace.com.au>
2023-05-20 01:44:02 +1000

    Add max 30 seconds

--- app/Main.tsx
+++ app/Main.tsx
@@ -7,8 +7,13 @@ export default function AudioInput() {
   const [convertedText, setConvertedText] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [copied, setCopied] = useState<boolean>(false);
-  const { startRecording, stopRecording, recordingBlob, isRecording } =
-    useAudioRecorder();
+  const {
+    startRecording,
+    stopRecording,
+    recordingBlob,
+    isRecording,
+    recordingTime,
+  } = useAudioRecorder();
 
   const sendAudio = useCallback(
     async (recordingBlob: Blob | null) => {
@@ -48,28 +53,39 @@ export default function AudioInput() {
     }
   }, [copied]);
 
+  useEffect(() => {
+    if (recordingTime >= 30) {
+      stopRecording();
+    }
+  }, [recordingTime, stopRecording]);
+
   return (
     <>
       <div className="p-8 flex-1 overflow-auto">
         {loading ? '...' : convertedText}
       </div>
-      <div className="border-t border-zinc-300 p-8 w-full bg-zinc-50 flex justify-center space-x-4">
-        <button
-          className="w-20 py-2 rounded bg-blue-500 text-white font-medium hover:bg-blue-600"
-          onClick={isRecording ? stopRecording : startRecording}
-        >
-          {isRecording ? 'Stop' : 'Record'}
-        </button>
-        <button
-          className="w-20 py-2 rounded bg-violet-500 text-white font-medium hover:bg-violet-600 disabled:opacity-50 disabled:cursor-not-allowed"
-          disabled={!convertedText}
-          onClick={() => {
-            navigator.clipboard.writeText(convertedText);
-            setCopied(true);
-          }}
-        >
-          {copied ? 'Copied' : 'Copy'}
-        </button>
+      <div className="border-t border-zinc-300 p-8 w-full bg-zinc-50 flex flex-col items-center">
+        <div className="space-x-4">
+          <button
+            className="w-20 py-2 rounded bg-blue-500 text-white font-medium hover:bg-blue-600"
+            onClick={isRecording ? stopRecording : startRecording}
+          >
+            {isRecording ? 'Stop' : 'Record'}
+          </button>
+          <button
+            className="w-20 py-2 rounded bg-violet-500 text-white font-medium hover:bg-violet-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={!convertedText}
+            onClick={() => {
+              navigator.clipboard.writeText(convertedText);
+              setCopied(true);
+            }}
+          >
+            {copied ? 'Copied' : 'Copy'}
+          </button>
+        </div>
+        <div className="mt-4 text-sm text-zinc-500">
+          You can record a maximum of 30 seconds
+        </div>
       </div>
     </>
   );
